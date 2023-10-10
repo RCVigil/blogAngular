d8f8874c532d5399fec905836e6600f596edab17
Rodrigo Cuervo <rodrigo_vigil@yahoo.com.br>
2023-10-09 21:30:15 -0300

    Feat: Incializando e finalizando o html dos componentes menu-bar, menu-title, big-card e samll-card

---
+++ src/app/Components/big-card/big-card.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-big-card',
+  templateUrl: './big-card.component.html',
+  styleUrls: ['./big-card.component.scss']
+})
+export class BigCardComponent {
+  lanternaFirst: string = 'https://th.bing.com/th/id/OIP.ZA3pgqj_SaPzK5Fnnm9ikAHaEK?w=321&h=180&c=7&r=0&o=5&pid=1.7'
+
+  facePicture: string = 'https://th.bing.com/th/id/OIP.weXnFJIY5611bJCfLe8LjAHaEK?w=282&h=180&c=7&r=0&o=5&pid=1.7'
+}
